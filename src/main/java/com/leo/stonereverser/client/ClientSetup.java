f25642316847c56fedfb8ec64f2e10b837c0c024
zenmeshenmmingdidoubunengyong <y15210442746@outlook.com>
2026-08-15 22:40:02 +0800

    Update ClientSetup.java

--- src/main/java/com/leo/stonereverser/client/ClientSetup.java
+++ src/main/java/com/leo/stonereverser/client/ClientSetup.java
@@ -8,7 +8,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
-@EventBusSubscriber(modid = StoneReverserMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = StoneReverserMod.MOD_ID, value = Dist.CLIENT)
 public class ClientSetup {
     @SubscribeEvent
     public static void registerScreens(RegisterMenuScreensEvent event) {
